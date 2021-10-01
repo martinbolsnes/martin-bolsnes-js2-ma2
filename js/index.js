c79c457d023b30228f3dd550be1c692417bda690
martinbolsnes <martinbolsnes@icloud.com>
2021-10-01 14:15:07 +0200

    added button that deletes from localstorage

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// imports always go on top
+import { testLengthOfInput } from './libs/validation.js';
+import writeToDOM from './libs/writeToDom.js';
+import {
+  saveToLocalStorage,
+  getStorageItem,
+} from './libs/localStorageHelper.js';
+
+const itemInput = document.querySelector('.itemInput');
+const addItem = document.querySelector('.addItem');
+const items = document.querySelector('.items');
+const deleteBtn = document.querySelector('.deleteBtn');
+let groceryArray = getStorageItem('groceryArrayKey');
+
+writeToDOM(items, groceryArray);
+
+addItem.onclick = function () {
+  let valueOfGroceryInputBox = itemInput.value;
+  if (testLengthOfInput(valueOfGroceryInputBox, 3)) {
+    let groceryItem = {
+      id: groceryArray.length,
+      name: valueOfGroceryInputBox,
+    };
+    groceryArray.push(groceryItem);
+    saveToLocalStorage('groceryArrayKey', groceryArray);
+    writeToDOM(items, groceryArray);
+  } else {
+    console.log('Input needs more characters');
+  }
+  itemInput.value = '';
+};
+
+function deleteFromLocalStorage() {
+  deleteBtn.onclick = function () {
+    localStorage.clear();
+    items.innerHTML = '';
+    groceryArray = [];
+  };
+}
+
+deleteFromLocalStorage();
